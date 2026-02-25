bf9a2add201648aed41fefd42bb668dbcec1b82a
Fawer Vargas <fawer5@gmail.com>
2026-02-25 17:53:47 +1100

    Changes to be committed: 	modified:   01-reforzamiento/src/main.ts

--- 01-reforzamiento/src/main.ts
+++ 01-reforzamiento/src/main.ts
@@ -1,9 +1,8 @@
 import './style.css'
-import './bases/06-d  '
+import './bases/06-deses-obj'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
 <h1>Vite + TypeScript</h1>
   </div>
 `
-
